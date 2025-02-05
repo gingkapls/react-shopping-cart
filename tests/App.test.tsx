1a72bb435f9535236ae09aeba5007818af52e474
gin <73906888+gingkapls@users.noreply.github.com>
2025-02-05 11:55:20 +0530

    feat: add tests

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,9 +1,11 @@
-import { render } from "@testing-library/react";
-import { describe, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
 import App from "../src/App";
 
-describe('App', () => {
-    it('renders headline', () => {
-        render(<App />)
-    })
-})
+describe("App", () => {
+  it("renders headline", () => {
+    render(<App title="React" />);
+    screen.debug();
+    expect(screen.getByRole("heading").textContent).toMatch(/Hello React/);
+  });
+});
