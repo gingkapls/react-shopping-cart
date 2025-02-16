facaf79b7b1f2394357aebab45a53d320789e968
gin <73906888+gingkapls@users.noreply.github.com>
2025-02-16 18:58:54 +0530

    feat(App): move default products to separate route

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,16 @@ import { useState } from 'react';
 import './App.css';
 import { Link, Outlet } from 'react-router';
 import { cart, cartItem } from './routes/CartPage';
-import { Category } from './lib/api';
 
 function App() {
   const [cart, setCart] = useState<cart>(new Map<number, cartItem>());
   const numCartItems = [...cart.values()].length;
-
-  const categories: Category[] = [
-    'electronics',
-    'jewelery',
-    "men's clothing",
-    "women's clothing",
-  ];
 
   return (
     <>
       <nav>
         <Link to='/'>Home</Link>
         <Link to='products'>Products</Link>
-        {categories.map((category) => (
-          <Link key={category} to={category}>
-            {category}
-          </Link>
-        ))}
         <Link to='cart'>Cart {numCartItems}</Link>
       </nav>
       <Outlet context={{ cart, setCart }} />
