34137ef4fb2240b4f18a73a690db5a493cb9f297
gin <73906888+gingkapls@users.noreply.github.com>
2025-02-16 19:09:22 +0530

    test(ProductCard): refactor tests

--- tests/ProductCard.test.tsx
+++ tests/ProductCard.test.tsx
@@ -2,69 +2,78 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { describe, expect, it, vi } from 'vitest';
 import ProductCard from '../src/components/ProductCard/ProductCard';
-import { Product } from '../src/lib/api';
 import { cartItem } from '../src/routes/CartPage';
 
-function setup(jsx: JSX.Element) {
+function setupProductCard() {
+  const mockItem = {
+    id: 1,
+    title: 'Laptop',
+    description: 'This is a laptop',
+    image: 'imageUrl',
+    price: 12.5,
+    category: 'electronics',
+    count: 0,
+  } satisfies cartItem;
+
+  const cart = new Map<number, cartItem>();
+  const setCart = vi.fn();
+
   return {
     user: userEvent.setup(),
-    ...render(jsx),
+    cart,
+    setCart,
+    mockItem,
+    ...render(<ProductCard product={mockItem} cart={cart} setCart={setCart} />),
   };
 }
 
-const mockItem = {
-  id: 1,
-  title: 'Laptop',
-  description: 'This is a laptop',
-  image: 'imageUrl',
-  price: 12.5,
-  category: 'electronics',
-  count: 0,
-} satisfies cartItem;
-
 describe('Product Card Component', () => {
-  it('displays product details when its not in cart', () => {
-    const cart = new Map<number, cartItem>();
-    const setCart = vi.fn();
+  describe('product details', () => {
+    it('displays product details when its not in cart', () => {
+      const { mockItem } = setupProductCard();
 
-    setup(<ProductCard product={mockItem} cart={cart} setCart={setCart} />);
-    
-    const image = screen.getByRole<HTMLImageElement>('img');
-    expect(image.src).toMatch(mockItem.image);
-    expect(image.alt).toMatch(mockItem.title);
+      const image = screen.getByRole<HTMLImageElement>('img');
+      expect(image.src).toMatch(mockItem.image);
+      expect(image.alt).toMatch(mockItem.title);
 
-    expect(screen.getByRole('heading').textContent).toMatch(mockItem.title);
+      expect(screen.getByRole('heading').textContent).toMatch(mockItem.title);
 
-    const priceText = screen.getByText('Price ' + '$' + mockItem.price);
-    expect(priceText).toBeInTheDocument();
+      const priceText = screen.getByText('Price ' + '$' + mockItem.price);
+      expect(priceText).toBeInTheDocument();
 
-    const buttonAdd = screen.getByRole('button', { name: /Add to Cart/i });
-    const buttonIncrement = screen.queryByRole('button', { name: /increment/i });
-    const buttonDecrement = screen.queryByRole('button', { name: /decrement/i });
+      const buttonAdd = screen.getByRole('button', { name: /Add to Cart/i });
+      const buttonIncrement = screen.queryByRole('button', {
+        name: /increment/i,
+      });
+      const buttonDecrement = screen.queryByRole('button', {
+        name: /decrement/i,
+      });
 
-    expect(buttonAdd).toBeInTheDocument();
-    expect(buttonIncrement).not.toBeInTheDocument();
-    expect(buttonDecrement).not.toBeInTheDocument();
+      expect(buttonAdd).toBeInTheDocument();
+      expect(buttonIncrement).not.toBeInTheDocument();
+      expect(buttonDecrement).not.toBeInTheDocument();
+    });
+
+    it('displays product details and relevant buttons when its in cart', () => {
+      const { mockItem } = setupProductCard();
+
+      expect(screen.getByRole('heading').textContent).toMatch(mockItem.title);
+
+      const priceText = screen.getByText('Price ' + '$' + mockItem.price);
+      expect(priceText).toBeInTheDocument();
+
+      const buttonAdd = screen.queryByRole('button', { name: /add to cart/i });
+      expect(buttonAdd).not.toBeInTheDocument();
+
+      const buttonIncrement = screen.getByRole('button', {
+        name: /increment/i,
+      });
+      const buttonDecrement = screen.getByRole('button', {
+        name: /decrement/i,
+      });
+      expect(buttonIncrement).toBeInTheDocument();
+      expect(buttonDecrement).toBeInTheDocument();
+    });
   });
 
-  it('displays product details and relevant buttons when its in cart', () => {
-    const cart = new Map<number, cartItem>();
-    cart.set(1, { ...mockItem, count: 1 });
-    const setCart = vi.fn();
-
-    setup(<ProductCard product={mockItem} cart={cart} setCart={setCart} />);
-
-    expect(screen.getByRole('heading').textContent).toMatch(mockItem.title);
-
-    const priceText = screen.getByText('Price ' + '$' + mockItem.price);
-    expect(priceText).toBeInTheDocument();
-
-    const buttonAdd = screen.queryByRole('button', { name: /add to cart/i });
-    expect(buttonAdd).not.toBeInTheDocument();
-    
-    const buttonIncrement = screen.getByRole('button', { name: /increment/i });
-    const buttonDecrement = screen.getByRole('button', { name: /decrement/i });
-    expect(buttonIncrement).toBeInTheDocument();
-    expect(buttonDecrement).toBeInTheDocument();
-  });
 });
